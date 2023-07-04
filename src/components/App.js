a214cda904c22d1c9b499dafc670819ed0741487
Richelle <richelle.ak@gmail.com>
2023-07-03 17:27:17 -0700

    add header, app, and multiply cocktail.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import NewMultiplyCocktailForm from "./MultiplyCocktail";
+
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <NewMultiplyCocktailForm />
+    </React.Fragment>
+  );
+}
+
+export default App;
