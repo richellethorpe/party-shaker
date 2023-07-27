307013a149980a9eeb90c5908706d7903a43618c
Richelle <richelle.ak@gmail.com>
2023-07-26 19:20:05 -0700

    new branch

---
+++ src/components/BatchControll.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {collection, addDoc } from "firebase/firestore"
+
+export default function BatchControll() {
+  return (
+    <div>
+      
+      const handleAddingNewRecipeTo
+    </div>
+  )
+}
