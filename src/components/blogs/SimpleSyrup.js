3f4db3064b5e2caa7d8cfbd479a8c834a777019f
Richelle <richelle.ak@gmail.com>
2023-07-26 21:49:25 -0700

    add simply syrup blog post

---
+++ src/components/blogs/SimpleSyrup.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function SimpleSyrup() {
+  return (
+    <div>
+      <h2>The Sweet Art of Making Simple Syrup: Elevating Your Beverage Game</h2>
+      <br />
+      <p>Have you ever taken a sip of a deliciously refreshing cocktail or mocktail and wondered how bartenders and mixologists manage to strike that perfect balance of sweetness? Chances are, they've utilized one of their secret weapons: simple syrup. This unassuming elixir is a game-changer when it comes to crafting impeccable beverages. Whether you're a budding home bartender or simply want to elevate your drinks at home, let's explore the art of making simple syrup.</p>
+      <br />
+      <h3>What is Simple Syrup?</h3>
+      <p>Simple syrup lives up to its name - it's a simple mixture of sugar and water. By dissolving sugar in water, you create a sweet, liquid form that seamlessly blends into cold beverages, ensuring a consistent distribution of sweetness.</p>
+      <br />
+      <h3>Why Use Simple Syrup?</h3>
+      <p>Using granulated sugar directly in cold beverages can lead to uneven sweetness, as it might not fully dissolve. This results in sugar settling at the bottom of your drink or a not-so-palatable grainy texture. Simple syrup overcomes these issues by providing a smoothly incorporated sweetness throughout the liquid, making it ideal for a wide range of drinks like cocktails, lemonades, iced teas, and coffee.</p>
+      <br />
+      <h3>The Classic Recipe: 1:1 Ratio</h3>
+      <p>The most common recipe for simple syrup involves a 1:1 ratio of sugar to water. It's straightforward to remember and quick to make.</p>
+      <p><em>Ingredients:</em></p>
+      <ul>
+        <li>1 cup granulated sugar</li>
+        <li>1 cup water</li>
+      </ul>
+
+      <p><em>Instructions:</em></p>
+      <ol>
+        <li>In a saucepan, combine equal parts of sugar and water.</li>
+        <li>Heat the mixture over medium heat, stirring constantly until the sugar completely dissolves.</li>
+        <li>Remove the saucepan from the heat and let the syrup cool down to room temperature.</li>
+        <li>Transfer the simple syrup to a clean, airtight container, such as a glass bottle or jar, and store it in the refrigerator for up to a month.</li>
+      </ol>
+    </div>
+  )
+}
